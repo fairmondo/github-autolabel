9b8cdc41456c44d9ee3dc15af907829793719f72
Andreas Knöpfle <andreas.knoepfle@gmail.com>
2014-11-07 16:25:14 +0100

    bug or refactoring labels now ready

--- application.rb
+++ application.rb
@@ -7,13 +7,19 @@ client = Octokit::Client.new(:access_token => ENV['GITHUB_TOKEN'])
 
 get '/' do
   logger.info "test"
+  binding.pry
   "Hello #{client.user}"
 end
 
 post '/' do
   if params["payload"]
-    logger.info "payload there"
-    logger.info JSON.parse(params["payload"])
-    logger.info JSON.parse(params["payload"]).class
+    payload = JSON.parse(params["payload"])
+    if payload["action"] == "labeled"
+	  issue = payload["issue"]["number"]
+	  label = payload["label"]["name"]
+	  if ["refactoring", "bug"].include? label
+		client.add_labels_to_an_issue("fairmondo/fairmondo",issue,["ready"])
+	  end
+    end
   end 
 end
